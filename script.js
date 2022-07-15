e3c1ed8fda358cb0b1b2ccb96599bd851735bc1a
Parsival Nascimento <92957914+Dev-Parsival@users.noreply.github.com>
2022-07-15 18:36:01 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,34 @@
+let contadorMenorEsqueda = document.querySelector(".btn_menos_adv1");
+let spam = document.querySelector("#spam");
+let contadorMaiorEsqueda = document.querySelector("#btn_mais_adv1");
+let cont = 0;
+let spam2 = document.querySelector("#spam2");
+spam.style.fontSize = "39px";
+spam2.style.fontSize = "39px";
+let h1 = document.querySelector("h1").style;
+h1.color = "#8AFF00";
+h1.fontSize = "56px";
+h1.textAlign = "center";
+
+
+function contaMais(){
+    cont += 1;
+    spam.innerText = cont;    
+}
+
+function contaMenos(){
+    cont -= 1;
+    spam.innerText = cont;    
+}
+
+function contaMaisD(){
+    cont += 1;
+    spam2.innerText = cont;    
+}
+
+function contaMenosD(){
+    cont -= 1;
+    spam2.innerText = cont;    
+}
+
+
